edea22161f77adc92f69206be9bc7bd3f429f3db
S1lent80 <silentcore32@gmail.com>
2016-11-28 12:51:52 -0500

    Add files via upload

---
+++ andx/chat_bot/filedir.py
@@ -0,0 +1,13 @@
+import os
+import sys
+import imp 
+
+## Python Module: File Check  -  Version: 1.0.0  -  Xver: 1.0 ##
+
+__version__ = '1.0.0'
+
+xver = 1.0
+
+def filecheck():
+    filecheck = raw_input("bot(files_dir)> ")
+    
